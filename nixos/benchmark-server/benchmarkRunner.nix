009343df039ea56651a65ed4a3c60219171b7eb6
Nic Flores <nic@slamdata.com>
2018-02-27 11:24:52 -0700

    remove github token and read a local file containing the token

--- nixos/benchmark-server/benchmarkRunner.nix
+++ nixos/benchmark-server/benchmarkRunner.nix
@@ -5,10 +5,11 @@ stdenv.mkDerivation rec {
   version = "0.6.3";
   assetId = "6286186";
 
+  githubToken = "${builtins.readFile /etc/nixos/githubtoken}";
   src = fetchurl {
     curlOpts = ["-L" "-H" "Accept:application/octet-stream"];
     # TODO: figure out how to not use a token here
-    url = "https://eab2f0e111c6235cee3f8f0c4394a377ab324e86:@api.github.com/repos/slamdata/slamdata-benchmarks/releases/assets/${assetId}";
+    url = "https://${githubToken}:@api.github.com/repos/slamdata/slamdata-benchmarks/releases/assets/${assetId}";
     sha256 = "ba2af69f6b37adce6c6368d001d5bd4f30c6e3aa19e13d276e43002a3a195f38";
   };
 
